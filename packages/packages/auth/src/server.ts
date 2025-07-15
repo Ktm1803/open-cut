f3661ec98b61bfe8c01b4f834230a1154aea88a8
Ktm1803 <106907541+Ktm1803@users.noreply.github.com>
2025-07-15 08:08:36 +0000

    a

---
+++ packages/packages/auth/src/server.ts
@@ -0,0 +1,30 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@opencut/db";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+  }),
+  secret: process.env.BETTER_AUTH_SECRET,
+  user: {
+    deleteUser: {
+      enabled: true,
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  appName: "OpenCut",
+  trustedOrigins: ["http://localhost:3000"],
+});
+
+export type Auth = typeof auth;
